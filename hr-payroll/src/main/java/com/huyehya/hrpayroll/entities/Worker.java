fe030e4a8834d1c11c3a8c4762e94b7f8fa4055e
Hussein Yehya <hussein.ali@becommerce.com.br>
2020-12-30 17:32:45 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/huyehya/hrpayroll/entities/Worker.java
@@ -0,0 +1,14 @@
+package com.huyehya.hrpayroll.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Worker {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
--- hr-payroll/src/main/java/com/huyehya/hrpayroll/services/PaymentsService.java
+++ hr-payroll/src/main/java/com/huyehya/hrpayroll/services/PaymentsService.java
@@ -1,12 +1,30 @@
 package com.huyehya.hrpayroll.services;
 
 import com.huyehya.hrpayroll.entities.Payments;
+import com.huyehya.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentsService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payments getPayments(Long workerId, Integer days){
-        return Payments.builder().name("Bob").dailyIncome(120.0).days(days).build();
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriVariables);
+
+        return Payments.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build();
     }
 }
