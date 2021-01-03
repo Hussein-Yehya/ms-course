deaabc1a523d8823ec6e280d72b8e2ebe3753376
Hussein Yehya <hussein.ali@becommerce.com.br>
2021-01-03 16:03:49 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/huyehya/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huyehya.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
