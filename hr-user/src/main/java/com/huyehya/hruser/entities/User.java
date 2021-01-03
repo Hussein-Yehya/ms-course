5d983a694c91f7140c9fc9e2b977905b2d743bcf
Hussein Yehya <hussein.ali@becommerce.com.br>
2021-01-03 15:44:29 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/huyehya/hruser/entities/User.java
@@ -0,0 +1,31 @@
+package com.huyehya.hruser.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
